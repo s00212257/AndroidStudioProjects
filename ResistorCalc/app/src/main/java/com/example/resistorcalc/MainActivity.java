b2a8ec61868ec377268f8db96d7cc2bb656de8df
Joseph <s00212257@atu.ie>
2022-10-20 12:15:39 +0100

    Button stuff added

--- ResistorCalc/app/src/main/java/com/example/resistorcalc/MainActivity.java
+++ ResistorCalc/app/src/main/java/com/example/resistorcalc/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.resistorcalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     Spinner spinner2;
     Spinner spinner3;
     Spinner spinner4;
+    Button btnCalc;
+    Button btnReset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
         spinner2 = findViewById(R.id.spinner2);
         spinner3 = findViewById(R.id.spinner3);
         spinner4 = findViewById(R.id.spinner4);
+
+        btnCalc = findViewById(R.id.btnCalc);
+        btnReset = findViewById(R.id.btnReset);
 
         ArrayAdapter<CharSequence> colourAdapter = ArrayAdapter.createFromResource(this,
                 R.array.colours_array, android.R.layout.simple_spinner_item);
@@ -36,6 +43,21 @@ public class MainActivity extends AppCompatActivity {
         spinner2.setAdapter(colourAdapter);
         spinner3.setAdapter(colourAdapter);
         spinner4.setAdapter(multiAdapter);
-    }
 
+        //Calc button press event
+        btnCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //Reset button press event
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
 }
