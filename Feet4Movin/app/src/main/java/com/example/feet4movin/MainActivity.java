75a28010982e199c441259d76ea12bc663bda633
Joseph <s00212257@atu.ie>
2022-10-20 11:34:04 +0100

    Resistor calc or whatever

--- Feet4Movin/app/src/main/java/com/example/feet4movin/MainActivity.java
+++ Feet4Movin/app/src/main/java/com/example/feet4movin/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.feet4movin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    Button button2;
+    Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "You picked the wrong one", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "You picked the wrong one", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "You picked the wrong one", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
     }
 }
